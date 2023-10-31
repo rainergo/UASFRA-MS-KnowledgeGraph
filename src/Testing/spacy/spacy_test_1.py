557ab067196b39d704d7e69589540d497bad2780
rainergo <rainer.gogel@web.de>
2023-10-31 17:18:53 +0100

    Data extraction from PDFs and display of entities in text in displaCy.

---
+++ src/Testing/spacy/spacy_test_1.py
@@ -0,0 +1,72 @@
+import re
+import spacy
+import pathlib
+from pprint import pprint
+
+from src.settings import path_data, reports
+
+#%%
+company = "Adidas"
+companies = [list_item['company'] for list_item in reports]
+assert company in companies
+with open(file=pathlib.Path(path_data, f"{company}_text_with_ents_clean.txt"), mode='r', encoding="UTF-8") as f:
+    text = f.read()
+
+#%%
+# Replacements
+replacements = {" us ": " " + company + " ",
+                " Us ": " " + company + " ",
+                "we are": company + " is",
+                "We are": company + " is",
+                " we ": " " + company + " ",
+                " We ": " " + company + " ",
+                " our ": " " + company + "'s ",
+                " Our ": " " + company + "'s "
+                }
+
+for old, new in replacements.items():
+    text = text.replace(old, new)
+
+pprint(text)
+print('---------------------------')
+
+#%%
+nlp = spacy.load("en_core_web_lg")
+nlp.add_pipe('coreferee')
+# nlp.add_pipe("xx_coref", config={"chunk_size": 2500, "chunk_overlap": 2, "device": 0})
+# doc_coref = nlp(text=text)
+# text = doc_coref._.resolved_text
+doc = nlp(text=text)
+
+#%%
+doc._.coref_chains.print()
+
+#%%
+sents_with_ents = list()
+sents_with_multiple_ents = list()
+ents_to_consider = ["PERSON", "ORG", "GPE"]
+doc = nlp(text=text)
+for sent in doc.sents:
+    entity_list = [(ent.label_, ent.text) for ent in sent.ents]
+    entity_to_consider_list = [(ent.label_, ent.text) for ent in sent.ents if ent.label_ in ents_to_consider]
+    if len(entity_to_consider_list) > 0:
+        print(f'ent_types: {entity_to_consider_list}\n'
+            f'sent:\n'
+              f'{sent.text}')
+        print('-----------------------------------------------------------')
+    if 0 < len(entity_list) < 2:
+        sents_with_ents.append(sent.text)
+    if 2 <= len(entity_list):
+        sents_with_multiple_ents.append(sent.text)
+
+#%%
+# Switch here:
+which_sents = sents_with_multiple_ents
+entity_sents = " ".join(which_sents)
+doc_ents = nlp(text=entity_sents)
+
+#%%
+styles = ["ent", "dep"]
+style = styles[0]
+from spacy import displacy
+displacy_output = displacy.serve(doc_ents, style=style, host="localhost", auto_select_port=True)
---
+++ src/settings.py
@@ -0,0 +1,14 @@
+import pathlib
+
+# PATHS
+path_base = pathlib.Path("D:/A_STUDIUM/PYTHON/UASFRA-MS-PROJDIGI/")
+path_data = pathlib.Path(path_base, "data/")
+path_pdfs = pathlib.Path(path_data, "pdfs/")
+
+# PDFs
+reports = [{"name": "covestro-ar22-entire.pdf", "company": "Covestro", "pages": [11, 12]},
+           {"name": "annual-report-adidas-ar22.pdf", "company": "Adidas", "pages": [x for x in range(79, 92)]},
+           {"name": "Airbus_Annual_Report_2022.pdf", "company": "Airbus", "pages": [x for x in range(61, 93)]},
+           {"name": "basf-ar22.pdf", "company": "BASF", "pages": [x for x in range(135, 151)]},
+            {"name": "entire-vw-ar22.pdf", "company": "BASF", "pages": [x for x in range(169, 175)]}
+           ]
